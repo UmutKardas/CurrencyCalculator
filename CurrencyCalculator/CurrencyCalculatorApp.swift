825a79265e133427725da6ab92aa502d76e600d6
Hüseyin Umut Kardaş <kardasumt@gmail.com>
2024-09-29 02:03:04 +0300

    Initial Commit

---
+++ CurrencyCalculator/CurrencyCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyCalculatorApp.swift
+//  CurrencyCalculator
+//
+//  Created by Hüseyin Umut Kardaş on 29.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
